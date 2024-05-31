53133f2d44a6722568c8f7fa7bfd38cf133b1881
ositooo1 <d48532356@alumnos.itr.edu.ar>
2024-05-31 09:35:08 -0300

    Empiezo y me canso

---
+++ main game.js
@@ -0,0 +1,18 @@
+let birdy;
+
+function setup(){
+
+    createCanvas(900, 600);
+    birdy = createSprite(
+      400, 540, 50, 50);
+    birdy.shapeColor = color(0);
+
+}
+
+function draw(){
+    background(250, 219, 216);
+    if (keyIsDown(65) === true) {
+        birdy.position.y += 10
+      }
+drawSprites();
+}
